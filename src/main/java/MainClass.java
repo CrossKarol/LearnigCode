706dfefa14966335f3611064f937dcf59c49ab6c
CrossKarol <karolekkawecki@o2.pl>
2019-03-01 00:29:36 +0100

    Initial commit.

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,24 @@
+public class MainClass {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+
+
+        for (int i = 0 ; i <= 10 ; i++)
+        {
+            System.out.println(i);
+        }
+        //String[] karl =  new String[5];
+
+        String[] karl = new String[]{"poniedziałek", "wtorek", "środa" ,"czwartek", "piątek", "sobota"};
+
+
+        for (int i = 0 ; i < karl.length ; i++)
+        {
+            System.out.println(karl[i]);
+        }
+    }
+
+
+
+}
